17a9ef63aa428617f138cc64d8bb3ab68c39505a
Thomas MacLean <thomas@weareantenna.be>
2026-04-27 10:34:04 +0200

    s

--- src/_data/colors.js
+++ src/_data/colors.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 module.exports = fs
-  .readFileSync(path.join(__dirname, '../../richter_256_colors.txt'), 'utf8')
+  .readFileSync(path.join(__dirname, "../../richter_256_colors.txt"), "utf8")
   .trim()
-  .split('\n')
-  .map(c => c.trim());
+  .split("\n")
+  .map((c) => c.trim());
